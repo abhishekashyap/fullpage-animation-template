bf27c22e762ebcb05111cd1f98b599fbbf527dd7
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-11-21 00:09:18 +0530

    Added image toggle

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 // TweenMax.defaultEase = Linear.easeOut;
 
-new fullpage('#fullPage', {
-    autoScrolling: true,
-    navigation: true,
-    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage'],
-})
+new fullpage("#fullPage", {
+  autoScrolling: true,
+  navigation: true,
+  anchors: ["firstPage", "secondPage", "thirdPage", "fourthPage", "fifthPage"],
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const div1 = document.getElementById("switch-1");
+  const div2 = document.getElementById("switch-2");
+
+  div2.style.display = "none";
+
+  const btn1 = document.getElementById("btn-1");
+  const btn2 = document.getElementById("btn-2");
+
+  // Toggle btn
+  btn1.addEventListener("click", function () {
+    div1.style.display = "block";
+    div2.style.display = "none";
+  });
+
+  btn2.addEventListener("click", function () {
+    div1.style.display = "none";
+    div2.style.display = "block";
+  });
+});
