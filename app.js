0931cc50495e634eaeb563db664a1412159b57d3
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-11-20 23:58:59 +0530

    Implemeneted fullpage

---
+++ app.js
@@ -0,0 +1,7 @@
+// TweenMax.defaultEase = Linear.easeOut;
+
+new fullpage('#fullPage', {
+    autoScrolling: true,
+    navigation: true,
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage'],
+})
